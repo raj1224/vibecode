3cf5a8df97652c8e039ad92e90c7f0102a1501a6
Raj Kumar <imraaj1026@gmail.com>
2025-11-21 17:02:08 +0530

    authentication is ongoing

---
+++ auth.config.ts
@@ -0,0 +1,18 @@
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+import type {NextAuthConfig} from "next-auth"
+import { auth } from "./auth"
+
+export default {
+    providers:[
+        Github({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+    ]
+}
---
+++ auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import {authConfig} from "./auth.config";
+import {db} from "@/lib/db"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+
+export const {auth, handlers, signIn, signOut} = NextAuth({
+    callbacks:{},
+    secret:process.env.AUTH_SECRET,
+    adapter:PrismaAdapter(db),
+    session:{strategy:"jwt"},
+    ...authConfig
+
+});
